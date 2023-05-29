a40d0b2cf5ff93ae94dd9f4ede748c5bee7edb00
joaoschrock <joaoschrock@gmail.com>
2023-05-29 18:58:33 -0300

    ATULIZACAO NAS PAGINAS

--- bilioteca/views/telaInicial.php
+++ bilioteca/views/telaInicial.php
@@ -14,12 +14,12 @@ $rowTable = $viewsLivros ->fetchAll();
     <link rel="stylesheet" href="../styles/inicial.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Prompt:wght@100&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Playfair:wght@600&family=Roboto:wght@500&display=swap" rel="stylesheet">
     <title>Tela Inicial</title>
 </head>
 <body>
     <div class="container">
-        <h1 id="Titulo">Sistema Organizacional da Biblioteca</h1>
+        <h1 id="Titulo">Biblioteca.com</h1>
         <div>
             <a href="../views/CadastroLIvros.php"><button id="Comprar" type="submit" class="btn">Cadastrar</button></a>
         </div>
